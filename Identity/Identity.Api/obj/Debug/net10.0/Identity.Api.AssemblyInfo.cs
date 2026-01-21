59daba117a688e43ab7445779c90455ce304d35c
valdamd <lava2345moduli@mail.ru>
2026-01-21 15:31:31 +0300

    обновление

--- Identity/Identity.Api/obj/Debug/net10.0/Identity.Api.AssemblyInfo.cs
+++ Identity/Identity.Api/obj/Debug/net10.0/Identity.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Identity.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb862dd2d0e3fdff83cba59f8368f2ab448bf3fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2288c9c360efc0204b0ac1cb3da88f8044b6e0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Identity.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Identity.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
